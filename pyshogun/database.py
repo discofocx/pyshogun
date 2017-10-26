2c390205f73e3a687e335f3bdb474502a476cbd7
discofocx <gsorchin@gmail.com>
2017-10-26 14:19:58 +0100

    Broke down the main function into sub-function calls depending on the "tree level" as a new "filecmp obj" needs to be created everytime. Isolated the "process (copy file / make dir)" function as this way we can track overall progress and show it to the user.

--- pyshogun/database.py
+++ pyshogun/database.py
@@ -1,8 +1,10 @@
 from __future__ import print_function
 
 import os
+import sys
 import filecmp
 import shutil
+import time
 
 DEBUG = True
 
@@ -30,6 +32,140 @@ def compare_directories(source_dir, target_dir):
     print('Created {0} new dirs'.format(len(comparison.left_only)))
 
 
+def _sync_project_level(comparison_obj):
+    """
+    Generate lists of new directories and new files at Project level
+    :type comparison_obj: file.cmp.dircmp object
+    :param comparison_obj: The comparison on where we operate
+    :return: set - (List of new folders, List of new files)
+    """
+    new_files = list()
+    new_dirs = list()
+
+    if len(comparison_obj.left_only) != 0:
+        for project_dir in comparison_obj.left_only:
+            if os.path.isdir(os.path.join(comparison_obj.left, project_dir)):
+                new_dirs.append(os.path.join(comparison_obj.right, project_dir))
+                # os.mkdir(os.path.join(comparison_obj.right, project_dir))
+                # new_dirs += 1
+            else:
+                new_files.append((os.path.join(comparison_obj.left, project_dir), os.path.join(comparison_obj.right)))
+                # shutil.copy(os.path.join(comparison_obj.left, project_dir),
+                #             os.path.join(comparison_obj.right, project_dir))
+                # new_files += 1
+
+    return new_dirs, new_files
+
+
+def _sync_shoot_day_level(project_obj):
+    """
+    Generate lists of new directories and new files at Shoot day level
+    :param project_obj:
+    :return: set - (List of new folders, List of new files)
+    """
+    new_files = list()
+    new_dirs = list()
+
+    if len(project_obj.subdirs) != 0:
+        for project_dir, project_obj in project_obj.subdirs.iteritems():
+            for shoot_dir in project_obj.left_only:
+                if os.path.isdir(os.path.join(project_obj.left, shoot_dir)):
+                    new_dirs.append(os.path.join(project_obj.right, shoot_dir))
+                    # os.mkdir(os.path.join(project_obj.right, shoot_dir))
+                    # new_dirs += 1
+                else:
+                    new_files.append((os.path.join(project_obj.left, shoot_dir), os.path.join(project_obj.right)))
+                    # shutil.copy(os.path.join(project_obj.left, shoot_dir), project_obj.right)
+                    # new_files += 1
+
+    return new_dirs, new_files
+
+
+def _sync_session_level(shoot_day_obj):
+    """
+    Generate lists of new directories and new files at Session level
+    :param shoot_day_obj:
+    :return: set - (List of new folders, List of new files)
+    """
+    new_files = list()
+    new_dirs = list()
+
+    for project_dir, project_obj in shoot_day_obj.subdirs.iteritems():
+        if len(project_obj.subdirs) != 0:
+            for shoot_day_dir, shoot_day_obj in project_obj.subdirs.iteritems():
+                for session_dir in shoot_day_obj.left_only:
+                    if os.path.isdir(os.path.join(shoot_day_obj.left, session_dir)):
+                        new_dirs.append(os.path.join(shoot_day_obj.right, session_dir))
+                        # os.mkdir(os.path.join(shoot_day_obj.right, session_dir))
+                        # new_dirs += 1
+                    else:
+                        new_files.append(
+                            (os.path.join(shoot_day_obj.left, session_dir), os.path.join(shoot_day_obj.right)))
+                        # shutil.copy(os.path.join(shoot_day_obj.left, session_dir), shoot_day_obj.right)
+                        # new_files += 1
+
+    return new_dirs, new_files
+
+
+def _sync_takes_level(takes_obj):
+    """
+    Generate lists of new directories and new files at Takes level
+    :param takes_obj:
+    :return: set - (List of new folders, List of new files)
+    """
+    new_files = list()
+    new_dirs = list()
+
+    for project_dir, project_obj in takes_obj.subdirs.iteritems():
+        for shoot_day_dir, shoot_day_obj in project_obj.subdirs.iteritems():
+            if len(shoot_day_obj.subdirs) != 0:
+                for session_dir, session_obj in shoot_day_obj.subdirs.iteritems():
+                    for take in session_obj.left_only:
+                        if os.path.isdir(os.path.join(session_obj.left, take)):
+                            new_dirs.append(os.path.join(session_obj.right, take))
+                            # os.mkdir(os.path.join(session_obj.right, take))
+                            # new_dirs += 1
+                        else:
+                            new_files.append((os.path.join(session_obj.left, take), os.path.join(session_obj.right)))
+                            # shutil.copy(os.path.join(session_obj.left, take), session_obj.right)
+                            # new_files += 1
+    return new_dirs, new_files
+
+
+def _process_sync(tree_index, tree, dirs, files):
+    """
+    Run the make dir and copy files actions salvaged from the sync operations
+    :param tree_index:
+    :param dirs:
+    :param files:
+    :return:
+    """
+
+    if len(dirs) == 0 and len(files) == 0:
+        print('Databases at ({level}) level are synced'.format(level=tree[tree_index]))
+    else:
+        for index, l_dir in enumerate(dirs):
+            os.mkdir(l_dir)
+            progress = ((index + 1) * 100) / len(dirs)
+            sys.stdout.write('\rDirectory Sync Progress at ({level}) level {progress}% - '.format(level=tree[tree_index],
+                                                                                               progress=progress))
+            # sys.stdout.flush()
+            if DEBUG:
+                time.sleep(0.2)
+        print('Created ({dirs}) new directories at ({level})'.format(dirs=len(dirs), level=tree[tree_index]))
+
+        for index, l_file in enumerate(files):
+            source_file, destination_file = l_file
+            shutil.copy(source_file, destination_file)
+            progress = ((index + 1) * 100) / len(files)
+            sys.stdout.write('\rFile Sync Progress at ({level}) level {progress}% - '.format(level=tree[tree_index],
+                                                                                          progress=progress))
+            # sys.stdout.flush()
+            if DEBUG:
+                time.sleep(0.2)
+        print('Created ({files}) new files at ({level})'.format(files=len(files), level=tree[tree_index]))
+
+
 def sync(source_database, target_database):
     """
     Entry function.
@@ -46,30 +182,26 @@ def sync(source_database, target_database):
               os.path.basename(target_database) + '.enf',
               'RCS', 'CVS', 'tags']
 
-    # Create a new comparison object
-    comparison = filecmp.dircmp(source_database, target_database, ignore=ignore)
-
-    # Show a report
-    print(comparison.report_full_closure())
-    print('Left Only', comparison.left_only)
+    database_tree = {0: 'Project',
+                     1: 'Shoot Day',
+                     2: 'Session',
+                     3: 'Takes'}
 
     # Attempt to sync
-    # Sync any new sub dirs at project level
-    if len(comparison.left_only) != 0:
-        for project_dir in comparison.left_only:
-            os.mkdir(os.path.join(target_database, project_dir))
-        print('Created {0} new sub dirs at project level'.format(len(comparison.left_only)))
-    else:
-        print('Databases are synced at project level')
-
-    # Sync any new sub dirs and files at shoot day level
-    if len(comparison.subdirs) != 0:
-        for project_dir, obj in comparison.subdirs.iteritems():
-            for shoot_dir in obj.left_only:
-                if os.path.isdir(os.path.join(source_database, project_dir, shoot_dir)):
-                    os.mkdir(os.path.join(target_database, project_dir, shoot_dir))
-                else:
-                    shutil.copy(os.path.join(source_database, project_dir, shoot_dir), os.path.join(target_database, project_dir))
-        print('Created {0} new sub dirs at shoot day level'.format(len(comparison.subdirs)))
-    else:
-        print('Databases are synced at shoot day level')
+    for index, level in database_tree.iteritems():
+        if index == 0:
+            project_comp = filecmp.dircmp(source_database, target_database, ignore=ignore)
+            dirs, files = _sync_project_level(project_comp)
+            _process_sync(index, database_tree, dirs, files)
+        elif index == 1:
+            shoot_day_comp = filecmp.dircmp(source_database, target_database, ignore=ignore)
+            dirs, files = _sync_shoot_day_level(shoot_day_comp)
+            _process_sync(index, database_tree, dirs, files)
+        elif index == 2:
+            session_comp = filecmp.dircmp(source_database, target_database, ignore=ignore)
+            dirs, files = _sync_session_level(session_comp)
+            _process_sync(index, database_tree, dirs, files)
+        elif index == 3:
+            take_comp = filecmp.dircmp(source_database, target_database, ignore=ignore)
+            dirs, files = _sync_takes_level(take_comp)
+            _process_sync(index, database_tree, dirs, files)
